73916202a178b6421ede6fdc5fa895a1a38f9024
epikt <epiktsu@gmail.com>
2018-08-03 23:31:04 -0400

    fixed problem with logging and commit of simple elements

--- app/__init__.py
+++ app/__init__.py
@@ -76,10 +76,10 @@ def create_app(config_class=Config):
         file_handler.setFormatter(logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s '
             '[in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
+        file_handler.setLevel(logging.DEBUG)
         app.logger.addHandler(file_handler)
 
-        app.logger.setLevel(logging.INFO)
+        app.logger.setLevel(logging.DEBUG)
         app.logger.info('cl-concerts-db startup')
     return app
 
