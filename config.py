82cf4bcbcca55813bd40a69bc0cf62bb33d71b6b
Johnny Reveco <epiktsu@gmail.com>
2019-09-15 22:59:25 -0300

    Added several missing dependencies. Improved configs description. Completed README.md

--- config.py
+++ config.py
@@ -5,10 +5,15 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 class Config(object):
+    # will be used for generating hashes for codification. Use any prase here    
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-real-secret-one'
+    # The database URL. It should be compatible with SQL Alchemy. It defauls to sqlite
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    # See https://flask-sqlalchemy.palletsprojects.com/en/2.x/signals/ . You'll probably keep it as is.
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # the following parameters correspond to the mail server configuration. This is for sending emails with the errors
+    # and backtraces to the admin (MAIL_RECEIVER). If you don't setup this, you can always directly look in the logs
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') in ['True' , 'true' ]
@@ -17,8 +22,11 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_SENDER = os.environ.get('MAIL_SENDER')    
     MAIL_RECEIVER = os.environ.get('MAIL_RECEIVER')    
+    # The number of items displayed by default in the pages (used for pagination)
     ITEMS_PER_PAGE = int(os.environ.get('ITEMS_PER_PAGE') or 5)
+    # where the uploaded files wil be stored. There an 'uploads' directory will be created
     UPLOADS_DEFAULT_DEST = basedir
+    # language on which is expected to have translations.
     LANGUAGES = ['es', 'en']
     def __init__(self):
        print(os.path.join(basedir, '.env'))    
--- tests.py
+++ tests.py
@@ -35,56 +35,6 @@ class UserModelCase(unittest.TestCase):
 #                                         'd4c74594d841139328695756648b6bd6'
 #                                         '?d=identicon&s=128'))
 #
-#    def test_follow(self):
-#        u1 = User(username='john', email='john@example.com')
-#        u2 = User(username='susan', email='susan@example.com')
-#        db.session.add(u1)
-#        db.session.add(u2)
-#        db.session.commit()
-#        self.assertEqual(u1.followed.all(), [])
-#        self.assertEqual(u1.followers.all(), [])
-#
-#        u1.follow(u2)
-#        db.session.commit()
-#        self.assertTrue(u1.is_following(u2))
-#        self.assertEqual(u1.followed.count(), 1)
-#        self.assertEqual(u1.followed.first().username, 'susan')
-#        self.assertEqual(u2.followers.count(), 1)
-#        self.assertEqual(u2.followers.first().username, 'john')
-#
-#        u1.unfollow(u2)
-#        db.session.commit()
-#        self.assertFalse(u1.is_following(u2))
-#        self.assertEqual(u1.followed.count(), 0)
-#        self.assertEqual(u2.followers.count(), 0)
-#
-#    def test_follow_posts(self):
-#        # create four users
-#        u1 = User(username='john', email='john@example.com')
-#        u2 = User(username='susan', email='susan@example.com')
-#        u3 = User(username='mary', email='mary@example.com')
-#        u4 = User(username='david', email='david@example.com')
-#        db.session.add_all([u1, u2, u3, u4])
-#
-#        # create four posts
-#        now = datetime.utcnow()
-#        p1 = Post(body="post from john", author=u1,
-#                  timestamp=now + timedelta(seconds=1))
-#        p2 = Post(body="post from susan", author=u2,
-#                  timestamp=now + timedelta(seconds=4))
-#        p3 = Post(body="post from mary", author=u3,
-#                  timestamp=now + timedelta(seconds=3))
-#        p4 = Post(body="post from david", author=u4,
-#                  timestamp=now + timedelta(seconds=2))
-#        db.session.add_all([p1, p2, p3, p4])
-#        db.session.commit()
-#
-#        # setup the followers
-#        u1.follow(u2)  # john follows susan
-#        u1.follow(u4)  # john follows david
-#        u2.follow(u3)  # susan follows mary
-#        u3.follow(u4)  # mary follows david
-#        db.session.commit()
 #
 #        # check the followed posts of each user
 #        f1 = u1.followed_posts().all()
