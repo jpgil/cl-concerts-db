217d311f7abd7195a9789c0e800edc5c3001c1f1
Juan Pablo Gil <juanpablogil@gmail.com>
2020-08-29 22:27:38 -0400

    Clean repertorio

--- app/public/search.py
+++ app/public/search.py
@@ -120,7 +120,7 @@ class SideBarFilters:
                 {'name': 'premier_type', 'type': 'select', 'placeholder': 'Estreno',
                     'values': SelectCached(PremiereType).get()
                 },
-                {'name': 'musical_piece', 'type': 'select2', 'placeholder': 'Obra', 'url': 'musicalpieces'},
+                {'name': 'musical_piece', 'type': 'select2', 'placeholder': 'Obra', 'url': 'musicalpieces?clean=True'},
                 {'name': 'instruments', 'type': 'select2',
                     'placeholder': 'Instrumentos', 'url': 'instruments'},
                 {'name': 'instrument_type', 'type': 'select', 'placeholder': 'Tipo de Instrumentos',
